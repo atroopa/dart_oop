bab10e08b269c727d7be16e1c4d440e1f76d5d88
atroopa <atroopa@gmail.com>
2021-11-11 14:04:15 +0330

    stream tutarial Compelete

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,10 @@
-import 'oop.dart';
 import 'dart:async';
-void main() async{
+import 'dart:io';
+import 'dart:convert';
+import 'oop.dart';
+main() async{
 
-  //
+  
   var student = Student();
   student.phonNumber_set = 123456789;
   
@@ -20,10 +22,34 @@ void main() async{
   print(shower);
   print(pride.showText());
   
-  //
-  loadSomeThings("Hello from Async Dart!").then((str) => print(str));
+  
+  var load = loadSomeThings("Hello from Async Dart!").then((str) => print(str));
   var waiter = await loadByDelay("Hello from Async Dart");
-  print(waiter);
+  
+  examplesStream(10).listen((int x) {
+    print("String : $x"); 
+    }
+    );
+
+  Stream strm = examplesStream(3000000);
+  await for (var s in strm){
+    File file = File('text.txt');
+    IOSink sink = file.openWrite(mode: FileMode.append);
+    sink.add(utf8.encode("$s\n"));
+    await sink.flush();
+    await sink.close();
+  }  
+
+  File file = File("text.txt");
+  Stream<List<int>> fileStream = file.openRead();
+  fileStream.transform(utf8.decoder).transform(LineSplitter()).listen(
+    (str) => {print(str) }, 
+    onDone: () => print("Done Reading File"),
+    onError: (e) => e.toString() 
+    );
+      
+
+
 
 }
 
@@ -48,7 +74,16 @@ Future<String> fetchUserOrder() =>
 
 
 Future<String> loadByDelay(String str) async {
-  await Future.delayed(Duration(seconds: 10));
+  await Future.delayed(Duration(seconds: 2));
   return("String : $str");
 
 }
+
+//stream
+Stream<int> examplesStream(int x) async* {
+  for(int i=0 ; i<=x ; i++){
+    yield i;
+  }
+}
+
+
